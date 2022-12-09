a3e98d968080875faebeb6f95e9c7b2d29e5c09f
mario <mariohgcte@gmail.com>
2022-12-09 18:04:22 +0100

    Initial Commit

---
+++ FindMyBurguerApp/FindMyBurguerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyBurguerAppApp.swift
+//  FindMyBurguerApp
+//
+//  Created by Apps2T on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FindMyBurguerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
